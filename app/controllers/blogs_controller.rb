973a98e8ca4ca83431f4a1db8241d650a681377e
fukusy <fuku1study@gmail.com>
2021-09-06 08:42:27 +0000

    [Finish]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
-  def index
+  def show
+    @blog = Blog.find(params[:id])
   end
 
-  def show
+  def index
+    @blogs = Blog.all
   end
 
   def new
@@ -10,9 +12,24 @@ class BlogsController < ApplicationController
   end
 
   def create
-
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
